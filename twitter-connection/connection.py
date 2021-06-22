f9fac56e7674590688e879a3c9e6e84d971e8ec6
Rinat Ibragimov (Rimov) <ruskirin@gmail.com>
2021-06-22 10:14:32 -0400

    Extract verb:stem pairs, update connection
    Add verb-stem-clean.txt containing dict of verb:stem pairs
    
    Update twitter-connection/connection.py with better handling of queries
    and fields
    
    twitter-extracting.ipynb contains updated way to query
    NOTE: not retrieving geo, and some other requested fields

--- twitter-connection/connection.py
+++ twitter-connection/connection.py
@@ -3,60 +3,81 @@ import requests
 
 
 class TwitterConnection:
-    def __init__(self, lang, cred_path):
+    # Option of supplying either a filepath to bearer token or the direct
+    #   string form of it; otherwise will default to predefined path
+    def __init__(self, lang, is_archive=False, cred_path='', bearer_token=''):
         self._langs = {"russian": "ru", "english": "en", "spanish": "es"}
+
+        self._prefix_search_recent = 'https://api.twitter.com/2/tweets/search/recent?query'
+        self._prefix_search_archive = 'https://api.twitter.com/2/tweets/search/all?query'
 
         self.lang = self._langs[lang] \
             if lang in self._langs.keys() \
             else self._langs["english"]
+        # Do a full-archive search?
+        self.prefix = self._prefix_search_recent if is_archive==False \
+                      else self._prefix_search_archive
 
-        # Text file with Bearer token saved as:
-        #   'BEARER ...'
-        self.cred_path = cred_path
+        # TXT filepath with Bearer token saved as:
+        #   'BEARER "XXXYYYZZZ..."'
+        # NOTE: must be prepended with 'BEARER'
+        self.cred_path = cred_path if len(cred_path)>0 \
+            else r'../twitter-connection/credentials.txt'
+
+        # Alternative option to giving a filepath
+        self.bearer_token = bearer_token
 
         self.header = self.create_headers()
+
+        self.query_cond = ''
+        self.fields_tweet = ''
+        self.fields_expan = ''
+        self.fields_user = ''
+        self.fields_place = ''
+
         self.url = ''
 
         self.response = None
         self.next_token = ''
 
-    # Retrieve bearer token from @self.cred_path
+    # If raw token was passed during object creation, use that
+    #   else look for a token in @cred_path
     def auth(self):
+        if len(self.bearer_token)>0:
+            return self.bearer_token
+
         try:
             with open(self.cred_path, 'r') as c:
                 f = c.read()
 
-                # TODO ??? : Add case where just token is passed instead to avoid
-                #   necessity for specific format of cred files
-                token = re.search(r'BEARER[\w\s]+\'?(.+)\'?\b', f).group(1)
+                token = re.search(r'BEARER[\w\s]+[\'\"]?(.+)[\'\"]?\b', f).group(1)
                 return token
         except IOError:
             print(f'Invalid file path: {self.cred_path}')
+        except Exception:
+            print(f'Invalid bearer token passed!')
 
     # Get proper format for bearer token
     def create_headers(self):
         return {'Authorization': f'Bearer {self.auth()}'}
 
-    # Combine query, fields and -if available- next_token into a proper URL
-    def create_url(self, query, fields, next_token=None):
-        prefix_search_recent = 'https://api.twitter.com/2/tweets/search/recent?query'
-
+    # Combine query, fields and (if available) next_token into a proper URL
+    def create_url(self, topic, next_token=None):
         if next_token is not None:
-            self.url = f'{prefix_search_recent}={query}&{next_token}&{fields}'
+            self.url = f'{self.prefix}={topic}' \
+                       f'&{next_token}' \
+                       f'&{self.fields_tweet}' \
+                       f'&{self.fields_expan}' \
+                       f'&{self.fields_user}' \
+                       f'&{self.fields_place}'
         else:
-            self.url = f'{prefix_search_recent}={query}&{fields}'
+            self.url = f'{self.prefix}={topic}' \
+                       f'&{self.fields_tweet}' \
+                       f'&{self.fields_expan}' \
+                       f'&{self.fields_user}' \
+                       f'&{self.fields_place}'
 
-    # !!! PRIVATE !!!
-    # Makes a get request and returns the response
-    def _connect_to_endpoint(self, url, headers):
-        response = requests.request('GET', url, headers=headers)
-
-        if response.status_code != 200:
-            raise ConnectionError(response)
-
-        return response
-
-    def connect(self, query, fields, is_next: bool = False):
+    def connect(self, query, is_next: bool = False):
         response = None
 
         # if is_next:
@@ -69,10 +90,9 @@ class TwitterConnection:
         #     self.create_url(query, fields)
 
         if is_next and (len(self.next_token) > 0):
-            self.create_url(query, fields,
-                            f'next_token={self.next_token}')
+            self.create_url(query, f'next_token={self.next_token}')
         else:
-            self.create_url(query, fields)
+            self.create_url(query)
 
         try:
             response = self._connect_to_endpoint(self.url, self.header)
@@ -83,6 +103,16 @@ class TwitterConnection:
 
         self.response = response.json()
         return self.get_next_token()
+
+    # !!! PRIVATE !!!
+    # Makes a get request and returns the response
+    def _connect_to_endpoint(self, url, headers):
+        response = requests.request('GET', url, headers=headers)
+
+        if response.status_code != 200:
+            raise ConnectionError(response)
+
+        return response
 
     def get_next_token(self):
         try:
@@ -97,3 +127,17 @@ class TwitterConnection:
             self.next_token = '' # Reset variable
 
             return False
+
+    def set_query(self, conditions=None):
+        if conditions is not None:
+            self.query_cond = conditions
+
+    def set_fields(self, tweet=None, expansions=None, user=None, place=None):
+        if tweet is not None:
+            self.fields_tweet = tweet
+        if expansions is not None:
+            self.fields_expan = expansions
+        if user is not None:
+            self.fields_user = user
+        if place is not None:
+            self.fields_place = place
